3684d0c30c9a70cadc61f217e1c66aa3630d0625
Allenshaji3 <allenshaji231515@gmail.com>
2019-05-29 21:00:05 -0500

    initial commit

---
+++ 200394891-CarRental/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_200394891_CarRental.MvcApplication" Language="C#" %>
---
+++ 200394891-CarRental/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _200394891_CarRental.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public String Brand { get; set; }
+        public String Model { get; set; }
+        public String Type { get; set; }
+        public int Year { get; set; }
+
+
+    }
+}
